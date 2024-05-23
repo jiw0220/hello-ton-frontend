f94e3a2e7ca20bfd74f2478d2887c571d4ab0489
jiw <jiwu_ma@qq.com>
2024-05-23 12:27:47 +0800

    ~

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,14 +2,19 @@
   <div class="page">
     <NButton type="primary" size="large" strong @click="onPressNext">Go Next Page</NButton>
     <NButton type="primary" size="large" strong @click="onPressEMCHub">Go EMC Hub</NButton>
+
+    <NButton type="primary" size="large" strong @click="onPressSet">Set Session Storage</NButton>
+
+    <NButton type="primary" size="large" strong @click="onPressGet">Get Session Storage</NButton>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { NButton } from 'naive-ui';
+import { NButton, useMessage } from 'naive-ui';
 const router = useRouter();
+const message = useMessage();
 function onPressNext() {
   router.push({ name: 'test' });
 }
@@ -17,7 +22,13 @@ function onPressNext() {
 function onPressEMCHub() {
   location.href = `https://emchub.ai/#/auth?launch=redirect&client=1234&redirect=https%3A%2F%2Fwww.baidu.com&scope=read`;
 }
-
+function onPressSet() {
+  sessionStorage.setItem('kk', 'hello?');
+}
+function onPressGet() {
+  const msg = sessionStorage.getItem('kk');
+  message.info(msg || 'none');
+}
 </script>
 
 <style scoped>
