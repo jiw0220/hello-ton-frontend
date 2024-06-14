22a51a7fcec2859da267868b3fc30d597bef67f8
jiw <jiwu_ma@qq.com>
2024-06-14 18:09:33 +0800

    ~

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,23 +3,44 @@
     <div>platform: {{ platform }}</div>
     <div>Telegram Mini App User: {{ userInfo }}</div>
     <NInput type="textarea" :value="authDataRaw"></NInput>
-    <NButton type="primary" size="large" strong @click="onPressNext">Go Next Page</NButton>
+    <NButton type="primary" size="large" strong @click="onPressSignForTelegram">Sign for telegram</NButton>
     <NButton type="primary" size="large" strong @click="onPressEMCHubPopup">Go EMC Hub Popup</NButton>
     <NButton type="primary" size="large" strong @click="onPressEMCHubRedirect">Go EMC Hub Redirect</NButton>
     <NButton type="primary" size="large" strong @click="onPressShowAlert">MiniAppAlert</NButton>
     <NButton type="primary" size="large" strong @click="onPressSet">Test Set Window.SessionStorage</NButton>
     <NButton type="primary" size="large" strong @click="onPressGet">Test Get Window.SessionStorage</NButton>
+    <NButton type="primary" size="large" style="margin-top: 24px" strong @click="onPressNext">Go Next Page</NButton>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { NButton, NInput, useMessage } from 'naive-ui';
+import { NButton, NInput, useMessage, useNotification } from 'naive-ui';
 import WebApp from '@twa-dev/sdk';
 import { retrieveLaunchParams } from '@tma.js/sdk';
 import { signInWithPopup, signInWithRedirect, getRedirectResult } from '@emcecosystem/emchub-auth-client';
 import { http } from '@/tools/http';
+
+//pc(windows): "tdesktop", pc(macos): "macos", mobile(ios): "ios", mobile(android): "android"
+//Not in telegram is "unknown"
+const platform = ref(WebApp.platform);
+const userInfo = ref('');
+const authDataRaw = ref('');
+const notification = useNotification();
+const router = useRouter();
+const message = useMessage();
+async function onPressSignForTelegram() {
+  if (!authDataRaw.value) {
+    notification.warning({ title: 'Failed', content: 'Please open in the mini app' });
+    return;
+  }
+  const resp = await http.postJSON({
+    url: 'https://dev.emchub.ai/api/v1/auth/telegram',
+    data: { raw: authDataRaw.value },
+  });
+  userInfo.value = JSON.stringify(resp.data);
+}
 
 async function onPressEMCHubPopup() {
   const options = { client: '1234' };
@@ -28,12 +49,6 @@ async function onPressEMCHubPopup() {
   const code = data.code;
   window.alert(`code: ${code}`);
 }
-
-//pc(windows): "tdesktop", pc(macos): "macos", mobile(ios): "ios", mobile(android): "android"
-//Not in telegram is "unknown"
-const platform = ref(WebApp.platform);
-const userInfo = ref('');
-const authDataRaw = ref('');
 
 async function onPressEMCHubRedirect() {
   const options = { client: '1234', redirect: location.href };
@@ -57,20 +72,12 @@ onMounted(async () => {
      *   await http.post({ url: 'backend url', data: { code } });
      */
   }
+
   const { initDataRaw } = retrieveLaunchParams();
   if (initDataRaw) {
     authDataRaw.value = initDataRaw;
-
-    const resp = await http.postJSON({
-      url: 'https://dev.emchub.ai/api/v1/auth/telegram',
-      data: { raw: initDataRaw },
-    });
-    userInfo.value = JSON.stringify(resp.data);
   }
 });
-
-const router = useRouter();
-const message = useMessage();
 
 function onPressNext() {
   router.push({ name: 'test' });
